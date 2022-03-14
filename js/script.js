95c4077cb684b7cf6c4f906fc2a80d62cb54e619
Rexcy Mosses aka RafLy <50251783+rafards@users.noreply.github.com>
2022-03-14 21:08:08 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+/* RAFLY START CODING JAVASCRIPT */
+/*===== Section Scroll Go Top Active Link =====*/
+// Ambil Button
+const goUpButton = document.querySelector(".go-up");
+
+const scrollContainer = () => {
+	return document.documentElement || document.body;
+};
+
+// Ketika user scroll kebawah sebanyak 100px perlihatkan buttonnya
+document.addEventListener("scroll", () => {
+	if (scrollContainer().scrollTop > 100) {
+		goUpButton.classList.remove("hide");
+	} else {
+		goUpButton.classList.add("hide");
+	}
+});
+/* RAFLY END JAVASCRIPT CODING */
 
